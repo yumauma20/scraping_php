6f1390d6b28a94dd3d0d75ebf8649cf1304e08fc
yumauma20 <71yuma10@gmail.com>
2021-08-21 17:07:39 +0900

    リンクの切り取り

--- app/Console/Commands/ScrapeMynavi.php
+++ app/Console/Commands/ScrapeMynavi.php
@@ -37,9 +37,11 @@ class ScrapeMynavi extends Command
      */
     public function handle()
     {
-      $crawler = \Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
-      $crawler->filter('.result__title .result__a')->each(function ($node) {
-        dump($node->text());
+      $url = "https://tenshoku.mynavi.jp/list/pg3/";
+      $crawler = \Goutte::request('GET', $url);
+      $crawler->filter('.cassetteRecruit__copy > a')->each(function ($node) {
+        $href = $node->attr("href");
+        dump(substr($href, 0, strpos($href, "/", 1) + 1));
       });
     }
 }
